ac8f897b6115a8d0318a10cf73e93af7afaa39a4
AlexKing90210 <Alex.king90@yandex.ru>
2022-10-10 15:52:31 +0400

    first try

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -11,11 +11,8 @@ def csrf_failure(request, reason=''):
     return render(request, 'core/403csrf.html')
 
 
-def server_error(request, exception):
-    # Переменная exception содержит отладочную информацию;
-    # выводить её в шаблон пользовательской страницы 500 мы не станем
-    return render(request, 'core/500.html', {'path': request.path}, status=404)
+def server_error(request):
+    return render(request, "core/500.html", status=500)
 
-
-def аorbidden_failure(request, reason=''):
-    return render(request, 'core/403.html')
+def permission_denied(request, exception):
+    return render(request, "core/403.html", status=403)
